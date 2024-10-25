868f45f34153f8ea6e08dd70c3e82bf5fe490036
RerezzOffc <thegamergemu0001@gmail.com>
2024-10-25 19:04:41 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Fungsi untuk membuka popup
+function openPopup(popupId) {
+    document.getElementById(popupId).style.display = "flex";
+}
+
+// Fungsi untuk menutup popup
+function closePopup(popupId) {
+    document.getElementById(popupId).style.display = "none";
+}
